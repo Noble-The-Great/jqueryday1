585fee5aa4900d0697c61796a57c7c4447f6eb04
Noble Boucon <nobleboucon@gmail.com>
2021-03-26 22:10:14 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+$('.navItem').css('color','white');
+
+$('#nav').css('background-color','#145');
+
+$('li:Contains("An even list item")').css('background-color','#ccc');
+
+$('li:Contains("An odd list item")').css('background-color','#aaa');
+
+
+  $('input').attr('placeholder','Noble Boucon');
